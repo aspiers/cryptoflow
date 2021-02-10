dd053c7386fb15ff104951ed6d87df0857bc32cb
Adam Spiers <crypto@adamspiers.org>
2021-02-10 23:24:38 +0000

    Add Transaction class

--- cryptoflow.py
+++ cryptoflow.py
@@ -2,80 +2,86 @@
 
 import csv
 import dateutil.parser
+import datetime
 import sys
 
-from pprint import pprint
-from operator import itemgetter
+from dataclasses import dataclass
+# from pprint import pprint
+# from operator import itemgetter
+
+
+@dataclass
+class Transaction:
+    date: datetime.datetime
+    sender: str
+    sent_amount: str
+    sent_currency: str
+    recipient: str
+    received_amount: str
+    received_currency: str
+    tx_type: str
+    tx_id: str
+
+    @property
+    def short_id(self):
+        return self.tx_id[0:4] + ".." + self.tx_id[-4:]
+
+    @property
+    def optional_id(self):
+        return f" {self.short_id}" if self.tx_id else ""
+
+
+class ExternalDeposit(Transaction):
+    pass
 
 
 class FlowAnalyser:
     def __init__(self):
         self.fundings_by_wallet = {}
+        self.swaps_by_wallet = {}
 
-    def add_txn(self, date,
-                sender, sent_amount, sent_currency,
-                recipient, received_amount, received_currency,
-                txtype, txid):
-        if not sender:
-            sender = "EXTERNAL"
-            assert (txtype == "crypto_deposit" or
-                    txtype == "fiat_deposit"), \
-                f"unexpected external deposit type f{txtype}"
+    def add_txn(self, txn):
+        if not txn.sender:
+            txn.sender = "EXTERNAL"
+            assert (txn.tx_type == "crypto_deposit" or
+                    txn.tx_type == "fiat_deposit"), \
+                f"unexpected external deposit type f{txn.tx_type}"
 
-        if not recipient or not received_amount:
+        if not txn.recipient or not txn.received_amount:
             return
 
-        if sender == recipient:
-            self.add_swap(
-                date,
-                sender, sent_amount, sent_currency,
-                received_amount, received_currency,
-                txtype, txid)
+        if txn.sender == txn.recipient:
+            self.add_swap(txn)
         else:
-            self.add_funding(
-                date,
-                sender, received_amount, received_currency,
-                recipient, received_amount, received_currency,
-                txtype, txid)
+            self.add_funding(txn)
 
-    def add_funding(self, date,
-                    sender, sent_amount, sent_currency,
-                    recipient, received_amount, received_currency,
-                    txtype, txid):
-        # txtype=crypto_deposit when sender is external
-        # txtype=transfer when sender is internal
-        if recipient not in self.fundings_by_wallet:
-            self.fundings_by_wallet[recipient] = []
+    def add_funding(self, txn):
+        # txn.tx_type=crypto_deposit when sender is external
+        # txn.tx_type=transfer when sender is internal
+        if txn.recipient not in self.fundings_by_wallet:
+            self.fundings_by_wallet[txn.recipient] = []
 
-        self.fundings_by_wallet[recipient].append(
-            [date, sender,
-             recipient, received_amount, received_currency]
-        )
-        print(f"{date} {sender} sent {recipient} "
-              f"{received_amount} {received_currency} "
-              f"({txtype}{optional_txid(txid)})")
+        self.fundings_by_wallet[txn.recipient].append(txn)
+        print(f"{txn.date} {txn.sender} sent {txn.recipient} "
+              f"{txn.received_amount} {txn.received_currency} "
+              f"({txn.tx_type}{txn.optional_id})")
 
-        if (received_currency != sent_currency or
-                received_amount != sent_amount):
-            print(f"!!! from {sent_amount} {sent_currency} !!!")
+        if ('deposit' not in txn.tx_type and
+            (txn.received_currency != txn.sent_currency or
+             txn.received_amount != txn.sent_amount)):
+            print(f"!!! from {txn.sent_amount} {txn.sent_currency} !!!")
 
-    def add_swap(self, date,
-                 wallet, sent_amount, sent_currency,
-                 received_amount, received_currency,
-                 txtype, txid):
-        # print(f"{date} {wallet}: swapped "
-        #       f"{sent_amount} {sent_currency} "
-        #       f"for {received_amount} {received_currency} "
-        #       f"({txtype}{optional_txid(txid)})")
-        pass
+    def add_swap(self, txn):
+        wallet = txn.recipient
 
+        if wallet not in self.swaps_by_wallet:
+            self.swaps_by_wallet[wallet] = []
 
-def short_txid(txid):
-    return txid[0:4] + ".." + txid[-4:]
-
-
-def optional_txid(txid):
-    return f" {short_txid(txid)}" if txid else ""
+        self.swaps_by_wallet[wallet].append(txn)
+        print(f"{txn.date} {wallet}: swapped "
+              f"{txn.sent_amount} {txn.sent_currency} "
+              f"for {txn.received_amount} {txn.received_currency} "
+              f"({txn.tx_type}{txn.optional_id})")
 
 
 class KoinlyFlowAnalyser:
@@ -96,27 +102,22 @@ class KoinlyFlowAnalyser:
 
     def analyse_txn(self, row):
         date = self.dateparser.parse(row['Date'])
-        (sender, sent_amount, sent_currency,
-         recipient, received_amount, received_currency,
-         txid, txtype) = itemgetter(
-            'Sending Wallet',
-            'Sent Amount',
-            'Sent Currency',
-            'Receiving Wallet',
-            'Received Amount',
-            'Received Currency',
-            'TxHash',
-            'Type'
-        )(row)
+        txn = Transaction(
+            date=date,
+            sender=row['Sending Wallet'],
+            sent_amount=row['Sent Amount'],
+            sent_currency=row['Sent Currency'],
+            recipient=row['Receiving Wallet'],
+            received_amount=row['Received Amount'],
+            received_currency=row['Received Currency'],
+            tx_id=row['TxHash'],
+            tx_type=row['Type'],
+        )
 
-        # if sender == recipient:
+        # if sender == txn.recipient:
         #     pprint(row)
 
-        self.analyser.add_txn(
-            date,
-            sender, sent_amount, sent_currency,
-            recipient, received_amount, received_currency,
-            txtype, txid)
+        self.analyser.add_txn(txn)
 
 
 def main():
