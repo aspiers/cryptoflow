4d19cdf79a59e93d6942738a02aaf5c6b0bafaba
Adam Spiers <crypto@adamspiers.org>
2021-03-03 18:35:33 +0000

    add utils.warn()

--- cryptoflow/utils.py
+++ cryptoflow/utils.py
@@ -5,5 +5,10 @@ import sys
 
 def abort(msg: str) -> None:
     sys.stdout.flush()
-    sys.stderr.write(f"Fatal: {msg}\n")
+    sys.stderr.write(f"FATAL: {msg}\n")
     sys.exit(1)
+
+
+def warn(msg: str) -> None:
+    sys.stdout.flush()
+    sys.stderr.write(f"WARNING: {msg}\n")
